576cb59e17ee8b4b728ad008ce15a36747e33d29
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2013-08-10 01:44:32 +0200

    fehler, wenn lehrer Ordner abgefragt, behoben.

--- app/folderImgPreview.php
+++ app/folderImgPreview.php
@@ -1,28 +1,31 @@
 <?php
 require_once('../auth.php');
 
+global $ne_config_info;
+
 $root = $ne_config_info['upload_dir'];
 $dir = urldecode($_REQUEST['dir']); //TODO sicherer daten bekommen? input::?
 $path = NavTools::root_filter($root . $dir);
 
 //TODO fehlerbehandlung?
-if(strcmp($path, "")==0){return;}
+if(empty($path)){return;}
 
 
 if (file_exists($path)) {
     if(!is_dir($path)){return;}
+    $returnLinksArray = array();
 
     $files = scandir($path);
 
     if (count($files) > 2) {
         natcasesort($files);
-        $returnLinksArray = array();
+
 
         foreach ($files as $file) {
             if (file_exists($path . $file) && $file != '.' && $file != '..' && !is_dir($path . $file)) {
                 $ext = preg_replace('/^.*\./', '', $file);
                 //TODO pics ext. arrary in config ?
-                if (in_array($ext, Array("jpg", "gif", "png", "jpeg"))) {
+                if (in_array($ext, Array('jpg', 'gif', 'png', 'jpeg'))) {
                     array_push($returnLinksArray, $dir . $file);
                 }
             }
