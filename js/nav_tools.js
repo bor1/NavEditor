3236dee7e607d2af99cd85f13b7f06ba6099d49a
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2013-11-17 04:33:38 +0100

    doppelte setting geloescht

--- js/nav_tools.js
+++ js/nav_tools.js
@@ -28,7 +28,6 @@ var NavTools = new function(){
         if( !s ){var s = {};}
         settings.current_host = location.protocol + "//" + location.host + "/";
         settings.nav_editor_path = self.ifsetor(s.nav_editor_path, 'vkdaten/tools/NavEditor3/');
-        settings.nav_editor_path = self.ifsetor(s.nav_editor_path, 'vkdaten/tools/NavEditor3/');
         settings.ajax_handler_path = self.ifsetor(s.ajax_handler_path, 'app/ajax_handler.php');
         settings.ajax_handler_fullpath = settings.current_host + settings.nav_editor_path + settings.ajax_handler_path;
     };
