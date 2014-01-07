0e8d0ba65b6b76da1f906d74c944b9eff6c2d63f
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2014-01-07 08:21:42 +0100

    new funcs: dirname, basename, pathinfo

--- js/nav_tools.js
+++ js/nav_tools.js
@@ -144,6 +144,166 @@ var NavTools = new function(){
         });
     };
 
+    /**
+     * dirname, php like
+     * {@link http://phpjs.org/functions/dirname/}
+     * @param {String} path path string to parse
+     * @example dirname('c:/Temp/x');<br/>returns 'c:/Temp'
+     * @example dirname('/dir/test/');<br/>returns '/dir'
+     * @returns {String} directory name
+     */
+    this.dirname = function(path) {
+        return path.replace(/\\/g, '/').replace(/\/[^\/]*\/?$/, '');
+    }
+
+
+    /**
+     * basename, php like
+     * {@link http://phpjs.org/functions/basename/}
+     * @param {String} path path string to parse
+     * @param {String} [suffix] extension to cut
+     * @example basename('/www/site/home.htm', '.htm');<br/>returns 'home'
+     * @example basename('ecra.php?p=1');<br/>returns 'ecra.php?p=1'
+     * @returns {String} file/folder name
+     */
+    this.basename = function(path, suffix) {
+        var b = path;
+        var lastChar = b.charAt(b.length-1);
+        if(lastChar === "/" || lastChar === "\\") {
+            b = b.slice(0, -1);
+        }
+
+        b = b.replace(/^.*[\/\\]/g, '');
+
+        if (typeof suffix === 'string' && b.substr(b.length - suffix.length) == suffix) {
+            b = b.substr(0, b.length - suffix.length);
+        }
+
+        return b;
+    }
+
+
+    /**
+     * Pathinfo, php like
+     * {@link http://phpjs.org/functions/pathinfo/}
+     * @param {String} path path to parse
+     * @param {mixed} options options:<br/>
+     *   'PATHINFO_DIRNAME': 1<br/>
+     *   'PATHINFO_BASENAME': 2<br/>
+     *   'PATHINFO_EXTENSION': 4<br/>
+     *   'PATHINFO_FILENAME': 8<br/>
+     *   'PATHINFO_ALL': 0
+     * @example pathinfo('/www/htdocs/index.html', 1);<br/>returns '/www/htdocs'
+     * @example pathinfo('/www/htdocs/index.html', 'PATHINFO_BASENAME')<br/>returns 'index.html'
+     * @example pathinfo('/www/htdocs/index.html', 2 | 4)<br/>returns {basename: 'index.html', extension: 'html'}
+     * @example pathinfo('/www/htdocs/index.html');<br/>returns {dirname: '/www/htdocs', basename: 'index.html', extension: 'html', filename: 'index'}
+     * @returns {String|Object} Object wih data, if no or multiple options set. Or String value if one option set
+     */
+    this.pathinfo = function(path, options) {
+        var opt = '',
+            optName = '',
+            optTemp = 0,
+            tmp_arr = {},
+            cnt = 0,
+            i = 0;
+        var have_basename = false,
+            have_extension = false,
+            have_filename = false;
+
+        // Input defaulting & sanitation
+        if (!path) {
+            return false;
+        }
+        if (!options) {
+            options = 'PATHINFO_ALL';
+        }
+
+        // Initialize binary arguments. Both the string & integer (constant) input is
+        // allowed
+        var OPTS = {
+            'PATHINFO_DIRNAME': 1,
+            'PATHINFO_BASENAME': 2,
+            'PATHINFO_EXTENSION': 4,
+            'PATHINFO_FILENAME': 8,
+            'PATHINFO_ALL': 0
+        };
+        // PATHINFO_ALL sums up all previously defined PATHINFOs (could just pre-calculate)
+        for (optName in OPTS) {
+            OPTS.PATHINFO_ALL = OPTS.PATHINFO_ALL | OPTS[optName];
+        }
+        if (typeof options !== 'number') { // Allow for a single string or an array of string flags
+            options = [].concat(options);
+            for (i = 0; i < options.length; i++) {
+                // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+                if (OPTS[options[i]]) {
+                    optTemp = optTemp | OPTS[options[i]];
+                }
+            }
+            options = optTemp;
+        }
+
+        // Internal Functions
+        var __getExt = function(path) {
+            var str = path + '';
+            var dotP = str.lastIndexOf('.') + 1;
+            return !dotP ? false : dotP !== str.length ? str.substr(dotP) : '';
+        };
+
+
+        // Gather path infos
+        if (options & OPTS.PATHINFO_DIRNAME) {
+            var dirname = this.dirname(path);
+            tmp_arr.dirname = dirname === path ? '.' : dirname;
+        }
+
+        if (options & OPTS.PATHINFO_BASENAME) {
+            if (false === have_basename) {
+                have_basename = this.basename(path);
+            }
+            tmp_arr.basename = have_basename;
+        }
+
+        if (options & OPTS.PATHINFO_EXTENSION) {
+            if (false === have_basename) {
+                have_basename = this.basename(path);
+            }
+            if (false === have_extension) {
+                have_extension = __getExt(have_basename);
+            }
+            if (false !== have_extension) {
+                tmp_arr.extension = have_extension;
+            }
+        }
+
+        if (options & OPTS.PATHINFO_FILENAME) {
+            if (false === have_basename) {
+                have_basename = this.basename(path);
+            }
+            if (false === have_extension) {
+                have_extension = __getExt(have_basename);
+            }
+            if (false === have_filename) {
+                have_filename = have_basename.slice(0, have_basename.length - (have_extension ? have_extension.length + 1 : have_extension === false ? 0 : 1));
+            }
+
+            tmp_arr.filename = have_filename;
+        }
+
+
+        // If array contains only 1 element: return string
+        cnt = 0;
+        for (opt in tmp_arr) {
+            cnt++;
+        }
+        if (cnt == 1) {
+            return tmp_arr[opt];
+        }
+
+        // Return full-blown array
+        return tmp_arr;
+    }
+
+
 
 
 
@@ -151,4 +311,4 @@ var NavTools = new function(){
     //set default settings
     this.set_settings();
 
-}();
+};
