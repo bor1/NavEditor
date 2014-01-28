aa9b1b8b429b14ceb3b566f8c3bf08ab3d56a4c4
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2014-01-28 03:02:38 +0100

    formatierung

--- common_nav_menu.php
+++ common_nav_menu.php
@@ -13,65 +13,66 @@ function createSubMenu($num)   {
 
     $link = '';
 	foreach ($ne_menu as $i => $v) {
-    		$class = '';
-    		$attribute = '';
-    		$desc = '';
-    		$key = $v['id'];
+        $class = '';
+        $attribute = '';
+        $desc = '';
+        $key = $v['id'];
 
-    		if ($num == $v['up']) {
-                        if($g_UserMgmt->isAllowAccessMenu($v['id'], $g_current_user_name)){
-				if ($actualPageName == $v['link']) {
-					$class .= 'active';
-				}
-				if (isset($v["sub"]) && $v['sub'] == 1) {
-					$class .= ' dropdown';
-				}
-				if (isset($v['addclass'])) {
-					$class .= ' '.$v['addclass'];
+        if ($num == $v['up']) {
+            if ($g_UserMgmt->isAllowAccessMenu($v['id'], $g_current_user_name)) {
+                if ($actualPageName == $v['link']) {
+                    $class .= 'active';
+                }
+                if (isset($v["sub"]) && $v['sub'] == 1) {
+                    $class .= ' dropdown';
+                }
+                if (isset($v['addclass'])) {
+                    $class .= ' ' . $v['addclass'];
 
-					if($v['addclass'] == "logout")
-						break;
-				}
-
-
-
-				if (isset($v['attribut'])) {
-					$attribute = $v['attribut'];
-				}
-				if (isset($v['desc']) && ($v['desc'])) {
-						$desc = 'title="'.$v['desc'].'"';
-				}
-
-				$link .= '<li';
-				if ($class) {
-					$link .= " class=\"$class\"";
-				}
-				$link .= ">";
-
-				if($v['sub'] == 1) {
-					$link .= '<a class="dropdown-toggle" data-toggle="dropdown" '.$desc.' '.$attribute.'  href="'.$v['link'].'">';
-				}else{
-						$link .= '<a '.$desc.' '.$attribute.'  href="'.$v['link'].'">';
-				}
-
-				$link .= $v['title'];
-
-			      if ($v['sub'] == 1) {
-					$link .= '<b class="caret"></b>';
-				}
-
-						$link .= '</a>';
+                    if ($v['addclass'] == "logout") {
+                        break;
+                    }
+                }
 
 
-			      if ($v['sub'] == 1) {
-			      	$link .= "<ul class=\"dropdown-menu\">\n";
-			      	$link .= createSubMenu($key);
-			      	$link .= "</ul>\n";
-				}
 
-	   		      $link .= "</li>\n";
-			}
-		}
+                if (isset($v['attribut'])) {
+                    $attribute = $v['attribut'];
+                }
+                if (isset($v['desc']) && ($v['desc'])) {
+                    $desc = 'title="' . $v['desc'] . '"';
+                }
+
+                $link .= '<li';
+                if ($class) {
+                    $link .= " class=\"$class\"";
+                }
+                $link .= ">";
+
+                if ($v['sub'] == 1) {
+                    $link .= '<a class="dropdown-toggle" data-toggle="dropdown" ' . $desc . ' ' . $attribute . '  href="' . $v['link'] . '">';
+                } else {
+                    $link .= '<a ' . $desc . ' ' . $attribute . '  href="' . $v['link'] . '">';
+                }
+
+                $link .= $v['title'];
+
+                if ($v['sub'] == 1) {
+                    $link .= '<b class="caret"></b>';
+                }
+
+                $link .= '</a>';
+
+
+                if ($v['sub'] == 1) {
+                    $link .= "<ul class=\"dropdown-menu\">\n";
+                    $link .= createSubMenu($key);
+                    $link .= "</ul>\n";
+                }
+
+                $link .= "</li>\n";
+            }
+        }
     }
     return $link;
 
@@ -84,7 +85,7 @@ function createSubMenu($num)   {
 		<div class="container">
 			<a class="brand" href="./index.php"><?php echo($ne_config_info['app_title']); ?></a>
 			<ul class="nav">
-				<?php echo createSubMenu(0);    ?>
+				<?php echo createSubMenu(0);?>
 			</ul>
 
 			<ul class="nav pull-right">
