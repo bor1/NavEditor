a57057ea481d6d7883135a8cf12c83ae95b35816
Barbara Bothe <barbara.bothe@fau.de>
2014-03-05 13:38:28 +0100

    Navbar responsiv gemacht (standard Bootstrap)

--- common_nav_menu.php
+++ common_nav_menu.php
@@ -83,13 +83,21 @@ function createSubMenu($num)   {
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
 		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 			<a class="navbar-brand" href="./index.php"><?php echo($ne_config_info['app_title']); ?></a>
 		</div>
-		<ul class="nav navbar-nav">
-			<?php echo createSubMenu(0);?>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="logout.php"><i class="glyphicon glyphicon-off"></i>&nbsp;Abmelden</a></li>
-		</ul>
+		<div class="collapse navbar-collapse" id="navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<?php echo createSubMenu(0);?>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="logout.php"><i class="glyphicon glyphicon-off"></i>&nbsp;Abmelden</a></li>
+			</ul>
+		</div>
 	</div>
 </nav>
