a9fa1f6d9df27dd4f4306568a0c86f19d33f6349
Simon Michalke <simon.m.m@freenet.de>
2014-11-13 00:12:44 +0100

    fixed merging issues

--- app/classes/ContentHandler.php
+++ app/classes/ContentHandler.php
@@ -1,170 +1,6 @@
 <?php
 // require config.php!
 class ContentHandler {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private $_defaultPath;
-    private $_filePath;
-    private $_templateHtml;
-    private $_title;
-    private $_content;
-    private $_newTemplateHtml;
-    private $_siteTitle;
-    private $_backupManager;
-    private $_content_block_pattern;
-    private $_content_block_pattern_fallback;
-    private $_content_marker_start;
-    private $_content_marker_end;
-    private $_content_marker_start_fallback;
-    private $_content_marker_end_fallback;
-    private $_content_marker_preinhaltsinfo;
-
-    function __construct($str_path) {
-        global $ne_config_info;
-
-        $website_conf = $this->getConfValues($ne_config_info['website_conf_filename']);
-        $this->_siteTitle = $website_conf['titel-des-Webauftritts'];
-        if($this->_siteTitle != '') {
-            $this->_siteTitle .= ': ';
-        }
-        $this->_defaultPath = $ne_config_info['app_path'] . 'data/templates/seitenvorlage.html';
-        if(!file_exists($this->_defaultPath)) {
-            $this->_defaultPath = $ne_config_info['app_path'] . 'data/templates/_seitenvorlage.html'; // fail-safe
-        }
-        $this->_newTemplateHtml = '';
-
-        $this->_content_block_pattern  = '/('.preg_quote($ne_config_info['page_content_marker_start']).')(.*?)('.preg_quote($ne_config_info['page_content_marker_end']).')/si';//match newlite too
-
-        $this->_content_block_pattern_fallback  = '/(<a name="contentmarke" id="contentmarke"><\/a>)(.*?)(<hr id="vorfooter" \/>)/si';
-        $this->_content_marker_start =  $ne_config_info['page_content_marker_start']; //   || '<a name="contentmarke" id="contentmarke"></a>';
-        $this->_content_marker_end =  $ne_config_info['page_content_marker_end']; //  || '<hr id="vorfooter" />';
-        // fuer alte Templateversionen:
-        $this->_content_marker_start_fallback  =  $ne_config_info['page_content_marker_start_fallback'];
-        $this->_content_marker_end_fallback =  $ne_config_info['page_content_marker_end_fallback'];
-
-                $this->_content_marker_preinhaltsinfo = $ne_config_info['page_content_marker_preinhaltsinfo'];
-
-        $this->_filePath = $str_path;
-        if(file_exists($this->_filePath)) {
-//            if(($this->_templateHtml = file_get_contents($this->_filePath)) == FALSE) {
-            if(($this->_templateHtml = $this->file_get_contents_utf8($this->_filePath)) == FALSE) {
-                throw new Exception('Unable to open file: ' . $this->_filePath);
-            }
-        } else { // try default path
-            if(($this->_templateHtml = file_get_contents($this->_defaultPath)) == FALSE) {
-                throw new Exception('Unable to open file: ' . $this->_filePath);
-            }
-        }
-    }
-
-    private function file_get_contents_utf8($fn) {
-        $content = file_get_contents($fn);
-        $content = str_ireplace('<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />', $content);
-        return mb_convert_encoding($content, 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', TRUE));
-    }
-
-    public function setBackupManager($backupManager) {
-        $this->_backupManager = $backupManager;
-    }
-
-    public function setContents($newTitle, $newContent) {
-        $this->_title = htmlentities($newTitle, ENT_COMPAT, 'UTF-8');
-        $this->_content = $newContent;
-        if($this->_content == '') {
-            $str_pattern = $this->_content_block_pattern;
-            preg_match($str_pattern, $this->_templateHtml, $matches);
-            if(count($matches)>2){
-                $this->_content = trim($matches[2]);
-            }
-        }
-    }
-
-    private function replaceTitles() {
-        // <title>...</title>
-        $pattern1 = '/<title>((\n|.)*?)<\/title>/i';
-        $this->_templateHtml = preg_replace($pattern1, '<title>' . $this->_siteTitle . $this->_title . '</title>', $this->_templateHtml);
-
-        // <h1>...</h1>
-        $pattern2 = '/(<div id="titel">)((\n|.)*?)(<\/div>)/i';
-        $this->_templateHtml = preg_replace($pattern2, '$1<h1>' . $this->_title . '</h1>$4', $this->_templateHtml);
-    }
-    private function replaceLogo() {
-        // <div id="logo">...</div>
-        global $ne_config_info;
-        $pattern = '%<div id="logo">.*?</div>%s';
-        $logoValues = $this->getConfValues($ne_config_info['website_conf_filename']);
-
-        If ($this->_filePath != $_SERVER['DOCUMENT_ROOT'].'/index.shtml'){
-            $hrefB = '<a href="/">';
-            $hrefE = '</a>';
-        }
-        If ($logoValues['kurzbeschreibung-zum-Webauftritt'] != ""){
-            $description = '<span class="description">'.$logoValues['kurzbeschreibung-zum-Webauftritt'].'</span>';
-        }
-        If ($logoValues['logo-URL'] != ""){
-            $logoAlt = ($logoValues['logo-Alt'] != "")?' alt="'.$logoValues['logo-Alt'].'"':'';
-            $logoW = ($logoValues['logo-Width'] != "")?' width="'.$logoValues['logo-Width'].'"':'';
-            $logoH = ($logoValues['logo-Height'] != "")?' height="'.$logoValues['logo-Height'].'"':'';
-            $logoImg = '<img'.$logoAlt.' src="'.$logoValues['logo-URL'].'"'.$logoW.$logoH.' border="0" />';
-        }
-        $namewebauftritt = "";
-        if ($logoValues['name-des-Webauftritts'] != "") {
-            $namewebauftritt = '<p>'.$hrefB.$logoValues['name-des-Webauftritts'].$hrefE.$description.'</p>';
-        } else {
-            if ($logoValues['kurzbeschreibung-zum-Webauftritt'] != ""){
-                 $namewebauftritt = '<p>'.$description.'</p>';
-            }
-        }
-
-
-        $logo = '<div id="logo">'.$hrefB.$logoImg.$hrefE.$namewebauftritt.'</div>';
-        $this->_templateHtml = preg_replace($pattern, $logo, $this->_templateHtml);
-    }
-
-    private function getConfValues($confFile) {
-    $fpath = $_SERVER['DOCUMENT_ROOT'] . '/vkdaten/'.$confFile;
-    $retv = array();
-    $to_concat = FALSE;
-    $fh = fopen($fpath, 'r') or die('Cannot open file!');
-    while(!feof($fh)) {
-        $oline = fgets($fh);
-        $pline = str_replace(array("\r", "\n", "\r\n"), '', ltrim($oline));
-        if((strlen($pline) == 0) || (substr($pline, 0, 1) == '#')) {
-            continue; // ignore comments and empty rows
-        }
-
-        if(substr($pline, strlen($pline) - 2, 2) == " \\") {
-            // concat next lines to form the value
-            if($to_concat === FALSE) {
-                $to_concat = TRUE;
-                $arr_opts1 = preg_split('/\t|\s{2,}/', $pline);
-                $opt1 = array($arr_opts1[0], str_replace(" \\", "", $arr_opts1[1]));
-                continue;
-            } else {
-                $opt1[1] .= " " . str_replace(" \\", "", $pline);
-            }
-        } else {
-            if($to_concat) {
-                $opt1[1] .= " " . $pline; // the last line
-                $retv[$opt1[0]] = $opt1[1];
-                //array_push($retv, $opt1);
-                $to_concat = FALSE;
-            } else {
-                $arr_opts = preg_split('/\t|\s{2,}/', $pline);
-                /*$opt = array(
-                    'opt_name' => $arr_opts[0],
-                    'opt_value' => $arr_opts[1]
-                );
-                array_push($retv, $opt); */
-                $retv[$arr_opts[0]] = $arr_opts[1];
-            }
-        }
-    }
-    fclose($fh);
-    return $retv;
-=======
-=======
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
     private $_defaultPath;
     private $_filePath;
     private $_templateHtml;
@@ -330,31 +166,10 @@ class ContentHandler {
         }
         fclose($fh);
         return $retv;
->>>>>>> aa546ac6e641d17a590e79a082d2423028ee9cac
     }
 
     private function replaceContent() {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
-        $str_pattern = $this->_content_block_pattern;
-        preg_match($str_pattern, $this->_templateHtml, $matches);
-        // error
-        if(count($matches) < 3) {
-            $str_pattern = $this->_content_block_pattern_fallback;
-
-        }
-<<<<<<< HEAD
-
-        // $str_pattern = $this->_content_block_pattern;
-        $this->_templateHtml = preg_replace($str_pattern, '$1' . "\n" . $this->_content . "\n" . '$4', $this->_templateHtml);
-    }
-
-    private function _replaceContentEx() {
-        $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start);
-=======
         $str_pattern = $this->_content_block_pattern;
         preg_match($str_pattern, $this->_templateHtml, $matches);
         // error
@@ -369,16 +184,6 @@ class ContentHandler {
 
     private function _replaceContentEx() {
         $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start);
->>>>>>> aa546ac6e641d17a590e79a082d2423028ee9cac
-=======
-
-        // $str_pattern = $this->_content_block_pattern;
-        $this->_templateHtml = preg_replace($str_pattern, '$1' . "\n" . $this->_content . "\n" . '$4', $this->_templateHtml);
-    }
-
-    private function _replaceContentEx() {
-        $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start);
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
                 $fallbackstart = mb_strpos($this->_templateHtml, $this->_content_marker_start_fallback );
                 $fallbackstart += mb_strlen($this->_content_marker_start_fallback );
                 $is_newstartpos = 0;
@@ -426,69 +231,6 @@ class ContentHandler {
                 if ($no_defaultendmarker==1) {
                     $part_tail = $this->_content_marker_end.$part_tail;
                 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        $this->_templateHtml = $part_head . "\n". $this->_content. "\n" . $part_tail;
-    }
-
-    // Do the replacement
-    public function ReplaceContents($backup = TRUE) {
-//        $this->replaceTitles();
-//        $this->_replaceContentEx();
-
-        if($backup) {
-            $this->_backupManager->backup($this->_filePath);
-        }
-        $this->SaveAsDraft();
-//        file_put_contents($this->_filePath, $this->_templateHtml);
-        $draft_path = $this->_filePath . '.buffer';
-        if(file_exists($draft_path)) {
-            copy($draft_path, $this->_filePath);
-            @unlink($draft_path);
-        }
-    }
-
-    public function SaveAsDraft() {
-        $draft_path = $this->_filePath . '.buffer';
-        if(file_exists($draft_path)) {
-            $this->_templateHtml = $this->file_get_contents_utf8($draft_path);
-        }
-
-        $this->replaceTitles();
-        $this->replaceLogo();
-        $this->_replaceContentEx();
-
-        file_put_contents($draft_path, $this->_templateHtml);
-    }
-
-    public function GetContentHtml() {
-        $pattern = $this->_content_block_pattern;
-        preg_match($pattern, $this->_templateHtml, $matches);
-        // error
-
-        if(count($matches) < 3) {
-            $pattern = $this->_content_block_pattern_fallback;
-            preg_match($pattern, $this->_templateHtml, $matches);
-            if(count($matches) < 3) {
-                return '<span style="color:red;">INFO: No content tags were found (' . count($matches) . '), this page is possibly not produced from templates.</span>';
-            }
-        }
-        $ret = $matches[2];
-        return $ret;
-    }
-
-    public function getContentHtmlEx() {
-        $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start);
-        $end_pos = mb_strpos($this->_templateHtml, $this->_content_marker_end);
-        $is_newstartpos = 0;
-        $fallbackstart;
-
-        $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start);
-                $fallbackstart = mb_strpos($this->_templateHtml, $this->_content_marker_start_fallback );
-                $fallbackstart += mb_strlen($this->_content_marker_start_fallback );
-=======
-=======
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
         $this->_templateHtml = $part_head . "\n". $this->_content. "\n" . $part_tail;
     }
 
@@ -545,38 +287,10 @@ class ContentHandler {
 
         $fallbackstart = mb_strpos($this->_templateHtml, $this->_content_marker_start_fallback );
         $fallbackstart += mb_strlen($this->_content_marker_start_fallback );
->>>>>>> aa546ac6e641d17a590e79a082d2423028ee9cac
 
         if ($start_pos !== FALSE) {
             $start_pos += mb_strlen($this->_content_marker_start);
                         $is_newstartpos = 1;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
-        } else {
-            $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start_fallback );
-            if ($start_pos !== FALSE) {
-                $start_pos += mb_strlen($this->_content_marker_start_fallback );
-            }
-
-        }
-
-<<<<<<< HEAD
-        $end_pos = mb_strpos($this->_templateHtml, $this->_content_marker_end);
-=======
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
-        if ($end_pos === FALSE) {
-            $end_pos = mb_strpos($this->_templateHtml, $this->_content_marker_end_fallback );
-        }
-
-        if($start_pos === FALSE || $end_pos === FALSE) {
-            return '<span style="color:red; font-size: 1.5em; font-weight: bold;">Der Textbereich der Seite konnte nicht ermittelt werden. <br />M&ouml;glicherweise wurden fehlerhafte HTML-Anweisungen eingef&uuml;gt, die das Analysieren des Textes verhindern. Bitte korrigieren Sie die fehlerhaften Bestandteile des Codes &uuml;ber den Quellcode-Editor bei dem Men&uuml;punkt Bilder und Dateien (f&uuml;r Administratoren) oder nutzen einen HTML-Editor.  </span>';
-        }
-<<<<<<< HEAD
-        $len = mb_strlen($this->_templateHtml);
-        $real_content = mb_substr($this->_templateHtml, $start_pos, $end_pos - $start_pos);
-=======
         } else {
             $start_pos = mb_strpos($this->_templateHtml, $this->_content_marker_start_fallback );
             if ($start_pos !== FALSE) {
@@ -594,11 +308,6 @@ class ContentHandler {
         }
 
         $real_content = mb_substr($this->_templateHtml, $start_pos, $end_pos - $start_pos);
->>>>>>> aa546ac6e641d17a590e79a082d2423028ee9cac
-=======
-
-        $real_content = mb_substr($this->_templateHtml, $start_pos, $end_pos - $start_pos);
->>>>>>> 1707af777649f237a76338f364f86f034ef5a20b
                 $textlen= mb_strlen($real_content);
                 if (($textlen < 1) && ($is_newstartpos)) {
                     //  use fallback
