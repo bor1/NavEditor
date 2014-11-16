6263ecd843725c65732af9819c0b19e3262fe69c
Simon Michalke <simon.m.m@freenet.de>
2014-11-16 21:27:31 +0100

    more cookies and credits to dmitry

--- fe_json/file_editor.json.php
+++ fe_json/file_editor.json.php
@@ -7,4 +7,4 @@
             
             var static_symbols_being_replaced = $.parseJSON('<?php echo(json_encode($ne_config_info['symbols_being_replaced'])); ?>');
             var static_symbols_replacement    = $.parseJSON('<?php echo(json_encode($ne_config_info['symbols_replacement'])); ?>');
-            var static_regex_removed_symbols  = <?php echo($ne_config_info['regex_removed_symbols']); ?>;
+            var static_regex_removed_symbols  = '<?php echo($ne_config_info['regex_removed_symbols']); ?>';
