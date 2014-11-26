28280e81898f4b624868e3d7030465d6efb4274b
Barbara Bothe <barbara.bothe@fau.de>
2014-11-26 14:24:04 +0100

    Areas Manager -> Bootstrap-Klassen

--- areas_manager.php
+++ areas_manager.php
@@ -3,9 +3,12 @@
             <script type="text/javascript">
                 $(document).ready(fe_areas_manager.loadContent);
             </script>
-            <div id="content_areas_manager">
-                <div id="areas_manager" >
-                    <div id="areasList"></div>
-                    <div id="areasSettings"></div>
+            <div id="content_areas_manager" class="container">
+				<div class="page-header">
+					<h3>Bereiche verwalten</h3>
+				</div>
+                <div id="areas_manager" class="row">
+                    <div id="areasList" class="col-md-3"></div>
+                    <div id="areasSettings" class="col-md-9 page"></div>
                 </div>
             </div>
