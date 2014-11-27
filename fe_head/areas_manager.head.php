9c3c3b867d7470d39b43f6f58d59199010b42100
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2014-11-27 02:08:56 +0100

    fix replace newlines

--- fe_head/areas_manager.head.php
+++ fe_head/areas_manager.head.php
@@ -8,10 +8,8 @@ $areas_manager_form_json = "";
 $areas_manager_form_json = file_get_contents($ne_config_info['fe_json_folder_name'] . '/' . 'areas_manager.json');
 
 
-///TODO string replace is not working!
-
-$areas_manager_form_json = str_replace('\n\r', '', $areas_manager_form_json);
-$areas_manager_form_json = str_replace('\n', '', $areas_manager_form_json);
+$areas_manager_form_json = str_replace("\n\r", '', $areas_manager_form_json);
+$areas_manager_form_json = str_replace("\n", '', $areas_manager_form_json);
 
 $areas_manager_list_html = "";
 
